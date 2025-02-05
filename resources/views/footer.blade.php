caf3d7179d454edf02d727d92b681ec08f13001e
George Hotten <github@ghott.me>
2025-02-05 01:04:52 +0000

    dev caution on footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,8 +1,12 @@
 <footer class="gweb-footer footer-sticky">
     <div class="footer-colours">
-        <div class="footer-colour" id="aston"></div>
-        <div class="footer-colour" id="solihull"></div>
-        <div class="footer-colour" id="george"></div>
-        <div class="footer-colour" id="hotten"></div>
+        @if(env('APP_ENV') == 'production')
+            <div class="footer-colour" id="aston"></div>
+            <div class="footer-colour" id="solihull"></div>
+            <div class="footer-colour" id="george"></div>
+            <div class="footer-colour" id="hotten"></div>
+        @else
+            <div class="footer-colour has-text-centered" id="george">Inwall is running in developer mode - some features may not act as expected.</div>
+        @endif
     </div>
 </footer>
